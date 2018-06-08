8a50e80d92606f7fe1b7ec48e0dc791c8b905c5d
Larry Memo <larrymemo9779@gmail.com>
2018-06-08 14:03:09 -0500

    v1.0.1

---
+++ server.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://larrymemo:Calle13num1@ds247290.mlab.com:47290/tallergaso");
+//mongodb://<dbuser>:<dbpassword>@ds247290.mlab.com:47290/tallergaso
